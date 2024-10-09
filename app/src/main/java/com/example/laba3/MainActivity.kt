5d7497cd67ea483a781feccd8147bbdaefef85e8
hai-mark <errrorfour@gmail.com>
2024-10-10 02:41:52 +0700

    Добавление элементов интерфейса

--- app/src/main/java/com/example/laba3/MainActivity.kt
+++ app/src/main/java/com/example/laba3/MainActivity.kt
@@ -6,7 +6,17 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var inputString: EditText
+    private lateinit var inputChar: EditText
+    private lateinit var findButton: Button
+    private lateinit var resultTextView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +26,7 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+
     }
 }
